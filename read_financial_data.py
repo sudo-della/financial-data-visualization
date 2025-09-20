046b5ea5117c34c92d4b7d328e0e01afc73af024
daniella <daniellamitsh@gmail.com>
2025-09-20 09:00:27 +0200

    made changes

--- read_financial_data.py
+++ read_financial_data.py
@@ -9,19 +9,30 @@ df = pd.read_excel('financial_data.xlsx', engine='openpyxl')
 print(df.head())
 
 
-conn = mysql.connector.connect(host = "localhost", 
-user= "root", 
-passwd= "computer", 
-database= "financial_data")
+conn = mysql.connector.connect(
+    host = "localhost", 
+    user= "root", 
+    passwd= "computer", 
+    database= "financial_data")
 
 cur = conn.cursor()
+
+for name in df["Name"].unique():
+    cur.execute("INSERT IGNORE into users (name) VALUES (%s)", (name,))
+
+    conn.commit()
+
+
+cur.execute("SELECT user_id, name FROM users")
+user_map = {name: user_id for user_id, name in cur.fetchall()}
+
 
 # insert rows into finiancial_recoreds table
 for index, row in df.iterrows():
     cur.execute("""
     INSERT INTO financial_records (user_id, year, month, amount)
     VALUES (%s, %s, %s, %s)
-    """), (row["user_id"], row["Year"], row["Month"],  row["Amount"]) 
+    """, (user_map[row["Name"]], row["Year"], row["Month"],  row["Amount"]))
 
 conn.commit()
 conn.close()
