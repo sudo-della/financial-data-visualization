b644a48ff950bcc944d2ecd33bb2f58202401d2b
sudo-della <daniellamitsh@gmail.com>
2025-10-03 16:38:54 +0200

    changed python to Java(springboot)

---
+++ src/main/java/com/example/financial_data_visualization/FinancialDataVisualizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.financial_data_visualization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialDataVisualizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialDataVisualizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/financial_data_visualization/FinancialDataVisualizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financial_data_visualization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialDataVisualizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
